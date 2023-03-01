bd01f1feb6da13e1c2875754374257eef6f369f8
Colton <allen.colton@hotmail.com>
2023-02-28 17:35:45 -0800

    set up environment

---
+++ main.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector('#game')
+const ctx = canvas.getContext('2d')
+
+sizeCanvas()
+
+
+
+function sizeCanvas(context) {
+  canvas.width = 800
+  canvas.height = 600
+}
